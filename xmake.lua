4e35b6628420b0aae7ebd293c558b725388cc11a
star-hengxing <hengxings783@gmail.com>
2022-12-04 17:52:21 +0800

    build: reduce binary size

--- xmake.lua
+++ xmake.lua
@@ -3,17 +3,20 @@ set_version("0.0.1")
 
 set_project("cs149")
 
+add_rules("mode.debug", "mode.releasedbg", "mode.release", "mode.minsizerel")
+
 set_arch("x64")
 set_warnings("all")
 set_languages("c++20")
-set_toolchains("clang")
-
-add_rules("mode.debug", "mode.releasedbg", "mode.release", "mode.minsizerel")
-
-add_requires("freeglut", {configs = {shared = true}})
 
 if is_os("windows") then
+    set_toolchains("clang-cl")
+    set_runtimes("MD")
     add_defines("_CRT_SECURE_NO_WARNINGS", "NOMINMAX")
+else
+    set_toolchains("clang")
 end
+
+add_requires("freeglut", {configs = {shared = true}})
 
 includes("src")
